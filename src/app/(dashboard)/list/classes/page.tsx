ab506846c8770cc8f38bdb2bae80a42d275a8639
cuijiawei123 <491758908@qq.com>
2024-09-11 17:28:28 +0800

    图片地址批量修改

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -71,10 +71,10 @@ const ClassListPage = () => {
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/filter.png" alt="" width={14} height={14} />
+              <Image src="/next_dashboard//filter.png" alt="" width={14} height={14} />
             </button>
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/sort.png" alt="" width={14} height={14} />
+              <Image src="/next_dashboard//sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && <FormModal table="class" type="create" />}
           </div>
