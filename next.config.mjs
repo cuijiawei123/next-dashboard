38890c4e5b4994df6bdcaf84e1cde380a461cb40
cuijiawei123 <491758908@qq.com>
2024-09-11 16:14:06 +0800

    添加basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: '/next_dashboard',
   images: {
     unoptimized: true, // 禁用图片优化功能
     remotePatterns: [{ hostname: "images.pexels.com" }],
